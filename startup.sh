6340a5b37e679cb9b9d4a70160808dada630ed23
karol-96 <karolbhandari666@gmail.com>
2025-02-13 17:28:21 +0545

    new changes on config

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/site/wwwroot
-pip install -r requirements.txt
-gunicorn -w 4 -k uvicorn.workers.UvicornWorker api_flask:app --bind=0.0.0.0:8000 --timeout 600
+source /home/site/wwwroot/antenv/bin/activate
+gunicorn --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000 app:app
