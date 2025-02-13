93a4dfbb6f782a853f4961ddb6a158540e2a0fbb
karol-96 <karolbhandari666@gmail.com>
2025-02-13 17:48:03 +0545

    new changes on config

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/site/wwwroot
-source /home/site/wwwroot/antenv/bin/activate
-gunicorn --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000 app:app
+source antenv/bin/activate
+python -m gunicorn app:app --bind=0.0.0.0:8000 --worker-class uvicorn.workers.UvicornWorker
