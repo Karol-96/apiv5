342793dacf4654ec7e7e0144157b635a22cd7009
karol-96 <karolbhandari666@gmail.com>
2025-02-13 17:25:42 +0545

    new changes on config

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,10 +1,41 @@
- # Gunicorn configuration
+import multiprocessing
+
+# Server socket
 bind = "0.0.0.0:8000"
-workers = 4
+backlog = 2048
+
+# Worker processes
+workers = multiprocessing.cpu_count() * 2 + 1
 worker_class = "uvicorn.workers.UvicornWorker"
+worker_connections = 1000
 timeout = 120
 keepalive = 5
+
+# Logging
 errorlog = "-"
+loglevel = "info"
 accesslog = "-"
+access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
+
+# Process naming
+proc_name = "fastapi_app"
+
+# Server mechanics
+daemon = False
+pidfile = None
+umask = 0
+user = None
+group = None
+tmp_upload_dir = None
+
+# SSL
+keyfile = None
+certfile = None
+
+# Debugging
+reload = False
+reload_engine = "auto"
+
+# Logging
 capture_output = True
 enable_stdio_inheritance = True
